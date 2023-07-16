87718ece5ecfad1f812a9cf567a943e6c4b876e1
jeongwwon <spwldmlgla@naver.com>
2023-07-16 20:45:56 +0900

    XML

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,18 +1,38 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+//구성 영역
+//역할과 구현을 확실하게 보여지도록 구분
+@Configuration
 public class AppConfig {
-    public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl(getMemberRepository());
     }
 
-    public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+    @Bean
+    public MemoryMemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(getMemberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
