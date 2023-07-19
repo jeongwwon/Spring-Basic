aa98e33113e30d78793a3f76a5470fa0fecb9444
jeongwwon <spwldmlgla@naver.com>
2023-07-19 16:34:40 +0900

    Proxy

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,12 +1,14 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+//qualifer 대신 사용자 지정 애노테이션을 사용하면 오류 발생시 쉽게 캐치할수 있다.
 @Component
-@Qualifier("mainDiscountPolicy")
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy{
     private int discountPercent=10;
 
